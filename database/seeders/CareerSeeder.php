4bdb596dd94116c9f43dc9f6406b7eb1dba6a23a
DerekSB0305 <dereksolorzano2111@gmail.com>
2025-09-08 14:20:02 -0600

    Seeder Carreras

--- database/seeders/CareerSeeder.php
+++ database/seeders/CareerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Career;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class CareerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Career::insert([
+            ['name' => 'Ingeniería Informatica', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Ingeniería Bioquímica', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Ingeniería Industrial', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Ingeniería Agronomía', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Ingeniería Administración', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Ingeniería Electromecánica', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Ingeniería Energías Renovables', 'study_plan' => '2025', 'created_at' => now(), 'updated_at' => now()]
+        ]);
     }
 }
