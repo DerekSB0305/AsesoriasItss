bf765b5897fe9adb20c33d40197f0ce6849599b4
AndresAVC <darkingacv24@gmail.com>
2025-09-08 13:52:33 -0600

    Carpeta

--- database/factories/DepartmentFactory.php
+++ database/factories/DepartmentFactory.php
@@ -17,10 +17,6 @@ class DepartmentFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->sentence(30),
-            'type' => fake()->sentence(30),
-            'location' => fake()->sentence(30),
-
         ];
     }
 }
