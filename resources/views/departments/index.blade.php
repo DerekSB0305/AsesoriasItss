99a6287cf670ceb32b1420fe4d3b618b88868bfd
AndresAVC <darkingacv24@gmail.com>
2025-09-09 11:16:18 -0600

    Crear departamento

---
+++ resources/views/departments/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista de Departamentos</title>
+</head>
+<body>
+    <h1>Lista de Departamentos</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Tipo</th>
+                <th>Ubicación</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($departments as $department)
+                <tr>
+                    <td>{{ $department->id }}</td>
+                    <td>{{ $department->name }}</td>
+                    <td>{{ $department->type }}</td>
+                    <td>{{ $department->location }}</td>
+                    <td>
+                        <a href="{{ route('departments.edit', $department) }}">Editar</a>
+                        <form action="{{ route('departments.destroy', $department) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
